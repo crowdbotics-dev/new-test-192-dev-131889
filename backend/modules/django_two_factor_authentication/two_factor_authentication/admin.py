bf08560ef32aeddd897727e811b2e2490c00a1bb
Dmitrii Alferev <dmitry@alferev.com>
2023-12-20 22:19:24 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- backend/modules/django_two_factor_authentication/two_factor_authentication/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import TwoFactorAuth, EnableTwoFactorAuthentication
-
-admin.site.register(TwoFactorAuth)
-admin.site.register(EnableTwoFactorAuthentication)
-
