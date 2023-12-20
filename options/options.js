e9d9aaab19a6333267ded25826cd2e8fbdbd373a
Dmitrii Alferev <dmitry@alferev.com>
2023-12-20 21:11:34 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "new-test-192-dev-131889",
-  url: "https://new-test-192-dev-131889.botics.co",
-  api: "https://new-test-192-dev-131889.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "new-test-192-dev-131889",
+  "url": "https://new-test-192-dev-131889.botics.co",
+  "api": "https://new-test-192-dev-131889.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
